2027cf6b8e7acd5341049e7daa63dd7449d2f88f
Léo Lebuhotel <leolebuhotel@MacBook-Pro-de-Leo-2.local>
2024-10-12 22:45:11 +0200

    Creation de la classe TextToSpeech

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,38 +1,35 @@
 #%%
 from google.cloud import texttospeech
 import io
-import pygame
 import random
-client = texttospeech.TextToSpeechClient()
+import base64
 
-audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.LINEAR16,
-    speaking_rate=1
-)
-voices = ["en-GB-Journey-D",
-"en-GB-News-K",
-"en-GB-Wavenet-A",
-"en-GB-Wavenet-F"]
+class TextToSpeech:
+    def __init__(self):
+        self.client = texttospeech.TextToSpeechClient()
+        self.audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.LINEAR16,
+            speaking_rate=1
+        )
+        self.voices = [
+            "en-GB-Journey-D",
+            "en-GB-News-K",
+            "en-GB-Wavenet-A",
+            "en-GB-Wavenet-F"
+        ]
 
-pygame.mixer.init()
-#%%
-def text_to_speech(text):
+    def text_to_speech(self, text):
+        voice_name = random.choice(self.voices)
+        input_text = texttospeech.SynthesisInput(text=text)
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="en-GB",
+            name=voice_name,
+        )
+        response = self.client.synthesize_speech(
+            request={"input": input_text, "voice": voice, "audio_config": self.audio_config}
+        )
 
-    voice_name = voices[random.randint(0, 3)]
-    input_text = texttospeech.SynthesisInput(text=text)
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-GB",
-        name=voice_name,
-    )
-    response = client.synthesize_speech(
-        request={"input": input_text, "voice": voice, "audio_config": audio_config}
-    )
+        audio_bytes = io.BytesIO(response.audio_content).read()
+        return base64.b64encode(audio_bytes).decode('utf-8')
 
-    audio_bytes = io.BytesIO(response.audio_content)
-    pygame.mixer.music.load(audio_bytes)
-    pygame.mixer.music.play()
-
-    # Wait for the audio to finish playing
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-#%%
+# %%
